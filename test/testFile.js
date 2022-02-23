22fc0de7e78c33f2fe7419f99e03fab3ce0e9229
Vinod Singh Bist <32848053+vinodbistvb@users.noreply.github.com>
2022-02-24 00:28:42 +0530

    Update testFile.js

--- test/testFile.js
+++ test/testFile.js
@@ -5,7 +5,7 @@ var assert = require('chai').assert
 describe('POST Groups (+) - Create Groups With Header Fields *end-to-end* *docker-test*', function () {
 
     it('C38812: Internal > Create Group > Validate SUCCESS message for group_name without path but with valid parent_group_id', async function () {
-
+    
         assert.equal(201, 201); 
     })
 })
