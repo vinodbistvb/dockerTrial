5dd89beea804706a0189a522a289ffe74cdb33d2
Vinod Singh Bist <32848053+vinodbistvb@users.noreply.github.com>
2022-02-23 23:40:49 +0530

    Create trial.test.js

---
+++ test/trial.test.js
@@ -0,0 +1,14 @@
+require('jest')
+
+var assert = require('chai').assert
+
+describe('POST Groups (+) - Create Groups With Header Fields *end-to-end* *docker-test*', function () {
+
+    it('C38812: Internal > Create Group > Validate SUCCESS message for group_name without path but with valid parent_group_id', async function () {
+
+        assert.equal(201, 201)
+        
+    })
+
+
+})
