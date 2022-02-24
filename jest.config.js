77231013b2b5fd69ac614f1cb76e19af928edc6e
Vinod Singh Bist <32848053+vinodbistvb@users.noreply.github.com>
2022-02-24 12:09:44 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,22 @@
+let currentdate = new Date(); 
+let datetime = currentdate.getDate() + "-"
+                + (currentdate.getMonth()+1)  + "-" 
+                + currentdate.getFullYear() 
+                // + "-"  
+                // + currentdate.getHours() + ":"  
+                // + currentdate.getMinutes() + ":" 
+                // + currentdate.getSeconds();                
+                // + currentdate.getSeconds();
+
+let fileName = datetime + "_testResults.xml";
+
+
 module.exports = {
     reporters: [
       'default',
       [ 'jest-junit', {
         outputDirectory: "./reports",
-        outputName: "testResults.xml",
-      } ] 
+        outputName: fileName
+      } ]
     ]
   };
