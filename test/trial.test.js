536045362b984f6722874e68b2d0978b73f0fe5e
Vinod Singh Bist <32848053+vinodbistvb@users.noreply.github.com>
2022-02-24 22:34:45 +0530

    Create trial.test.js

---
+++ test/trial.test.js
@@ -0,0 +1,25 @@
+require('jest')
+
+var assert = require('chai').assert
+
+describe('Running a dummy test set one', function () {
+
+    it('Dummy test one in set one', async function () {
+        assert.equal(201, 201)  
+    })
+
+    it('should fail - Dummy test Two in set one', async function () {
+        assert.equal(201, 200)  
+    })
+})
+
+describe('Running a dummy test set Two', function () {
+
+    it('should fail - Dummy test one in set two', async function () {
+        assert.equal(201, 200)  
+    })
+
+    it('Dummy test Two in set two', async function () {
+        assert.equal(201, 201)  
+    })
+})
