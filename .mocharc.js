4531448ee2c36c002d1c438083e78542bb825ad5
Vinod Singh Bist <32848053+vinodbistvb@users.noreply.github.com>
2022-03-31 20:16:23 +0530

    Create .mocharc.js

---
+++ .mocharc.js
@@ -0,0 +1,27 @@
+let currentdate = new Date(); 
+let datetime = currentdate.getDate() + "-"
+                + (currentdate.getMonth()+1)  + "-" 
+                + currentdate.getFullYear() 
+                // + "-"  
+                // + currentdate.getHours() + ":"  
+                // + currentdate.getMinutes() + ":" 
+                // + currentdate.getSeconds();                
+                // + currentdate.getSeconds();
+
+let fileName = datetime + "_testResults.xml";
+
+
+module.exports = {
+    timeout: 300000,
+    slow: 10000,
+    recursive: true,
+    reporter: 'mocha-junit-reporter',
+    reporterOptions: {
+        mochaFile: `./reports/${fileName}`,
+     
+    },
+    // require: [
+    //   'dotenv/config',
+    //   './test/globalHooks.js'
+    // ]
+  };
